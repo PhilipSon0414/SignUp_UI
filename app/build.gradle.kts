cb8507cbc871b6510221331f379cc7aeb8638f6c
Philip Son <jidongson@icloud.com>
2024-01-16 06:34:21 +0900

    image까지 넣음

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.jpapa.signup_ui"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jpapa.signup_ui"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
